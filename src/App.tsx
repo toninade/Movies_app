7d14b1bbd39595d6468412669ec2b168c0ef51d6
Toni <toninade10@gmail.com>
2025-12-06 10:05:56 +0200

    1.4

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
         <Route path="/nowplaying" element={<NowPlaying />} />
         <Route path="/popular" element={<Popular />} />
         <Route path="/tv" element={<TvShows />} />
