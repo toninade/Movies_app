2778ea77fe829f14f86e29f39a8ec2f65a9b83bd
Toni <toninade10@gmail.com>
2025-12-06 12:15:59 +0200

    1.5

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./sass/base/reset.scss";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NowPlaying from "./pages/NowPlaying";
 import Popular from "./pages/Popular";
 import TvShows from "./pages/TvShows";
@@ -11,13 +11,15 @@ import TvShows from "./pages/TvShows";
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/nowplaying" element={<NowPlaying />} />
-        <Route path="/popular" element={<Popular />} />
-        <Route path="/tv" element={<TvShows />} />
-      </Routes>
+      <Router basename="/Movies_app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/nowplaying" element={<NowPlaying />} />
+          <Route path="/popular" element={<Popular />} />
+          <Route path="/tv" element={<TvShows />} />
+        </Routes>
+      </Router>
     </>
   );
 }
